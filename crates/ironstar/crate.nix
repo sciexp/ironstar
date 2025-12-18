62021c9d962c117b19b94fefc9f9566b88117d69
Cameron Smith <cameron.ray.smith@gmail.com>
2025-12-18 17:21:49 -0500

    fix(nix): simplify crate.nix to minimal dependencies

--- crates/ironstar/crate.nix
+++ crates/ironstar/crate.nix
@@ -4,22 +4,12 @@
   autoWire = [ ];
   crane = {
     args = {
-      buildInputs =
-        lib.optionals pkgs.stdenv.isDarwin [
-          pkgs.darwin.IOKit
-          pkgs.darwin.Security
-          pkgs.darwin.SystemConfiguration
-          pkgs.darwin.CF
-        ]
-        ++ lib.optionals pkgs.stdenv.isLinux [
-          pkgs.openssl
-        ];
+      buildInputs = lib.optionals pkgs.stdenv.isLinux [
+        pkgs.openssl
+      ];
 
       nativeBuildInputs = [
         pkgs.pkg-config
-      ]
-      ++ lib.optionals pkgs.stdenv.isDarwin [
-        pkgs.darwin.Cocoa
       ];
     };
   };
