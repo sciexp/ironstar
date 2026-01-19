8ceb6e404fbe725bad0084075e835d56133e1df9
Cameron Smith <cameron.ray.smith@gmail.com>
2026-01-19 14:34:25 -0500

    feat(frontend): add web-components entry point stub

---
+++ web-components/index.ts
@@ -0,0 +1,8 @@
+// web-components/index.ts
+// Entry point for frontend assets
+// CSS import will be processed by Rolldown + PostCSS (configured in ny3.2, ny3.3)
+
+import "./styles/main.css";
+
+// Web components will be registered here (ny3.8, ny3.14)
+// Type exports will be added here (ny3.10)
